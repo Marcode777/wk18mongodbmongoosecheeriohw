4ab43c33e0576f2f755bc3a2d829934f14c7a987
Marcode777 <marc102202@hotmail.com>
2016-03-14 21:38:05 -0400

    implemented cheerio and successfully scraped header tags from bloomberg.com home page

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var expressHandleBars = require("express-handlebars");
 var mongoose = require("mongoose");
-var cheerio = require("cheerio");
+var cheerio = require("cheerio");// npm cheerio makes even simpler use of jQuery features
 var request = require("request"); //npm request is designed to be the simplest way to make http calls (go to the internet and get data back)
 var mongojs = require("mongojs");
 
@@ -11,6 +11,10 @@ var mongojs = require("mongojs");
 console.log("this works");
 request("http://bloomberg.com", function(error, response, body){
   if(!error && response.statusCode == 200){
-    console.log(body)
-  }
+    $ = cheerio.load(body);
+
+    $("h1").each(function(i, elem){
+      console.log($(this).text());
+    })
+  };
 });
