6eb03d54bf67cb7a5e5a539e03c65b64ca19ad4f
Marcode777 <marc102202@hotmail.com>
2016-03-14 21:13:10 -0400

    tested request in server.js, functional

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var expressHandleBars = require("express-handelbars");
+var expressHandleBars = require("express-handlebars");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
-var request = require("request");
+var request = require("request"); //npm request is designed to be the simplest way to make http calls (go to the internet and get data back)
 var mongojs = require("mongojs");
 
 
+console.log("this works");
+request("http://bloomberg.com", function(error, response, body){
+  if(!error && response.statusCode == 200){
+    console.log(body)
+  }
+});
